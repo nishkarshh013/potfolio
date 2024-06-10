43d33d820b74c52e345c6d2f8c7339046ebffaaa
Nishkarsh Sahu <nishkarshsahu007@gmail.com>
2024-06-10 18:28:10 +0530

    Change stimulus.js

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,9 +1,12 @@
 import { Application } from "@hotwired/stimulus"
+import { definitionsFromContext } from "stimulus/webpack-helpers";
 
 const application = Application.start()
+const context = require.context("./controllers", true, /\.js$/);
 
 // Configure Stimulus development experience
 application.debug = false
 window.Stimulus   = application
 
 export { application }
+application.load(definitionsFromContext(context));
