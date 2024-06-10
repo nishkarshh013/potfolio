9de3875b950bd0c05b098a8039ecc0b15d119630
Nishkarsh Sahu <nishkarshsahu007@gmail.com>
2024-06-10 16:53:44 +0530

    Routes issue

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,4 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "profiles#index"
   get "/experience", to: "profiles#experience", as: :experience
-  # get "/skills", to: "profiles#skills", as: :skills
-  # get "/education", to: "profiles#education", as: :education
-  # get "/contact", to: "profiles#contact", as: :contact
-
-  # get "/back", to: "profiles#back", as: :back
 end
